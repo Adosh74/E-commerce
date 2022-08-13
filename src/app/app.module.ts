645ba00d933effb488abef8cb1ddb36e2b18fc3f
Mohamed Hamdy <hamdysheblm@gmail.com>
2022-08-13 17:05:32 +0200

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CartsComponent } from './carts/carts.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    CartsComponent,
+    NavBarComponent,
+    FooterComponent,
+    ContentComponent,
+    ProductCardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
